293384bf467f145d9063d4aba0d63dfd8f6dd41f
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2023-05-20 14:16:49 -0700

    Add get conversion by form

--- scripts.js
+++ scripts.js
@@ -1,14 +1,32 @@
 let currencyAPIKey = "BXEh8fPVQeAN2jijFixukfmK2PNmKo83";
-let theAmount = document.getElementById("amount");
-let fromCurrency = document.getElementById("from");
-let toCurrency = document.getElementById("to");
+let theAmount = document.getElementById("amount").value;
+let fromCurrency = document.getElementById("from").value;
+let toCurrency = document.getElementById("to").value;
 let theAnswer = document.getElementById("answer");
 
-window.onload = getData();
+// window.onload = getData();
 
-async function getData() {
+/**
+ * On form submission, provide the conversion result
+ * @param {*} amountInput 
+ * @param {*} fromInput 
+ * @param {*} toInput 
+ */
 
-    const apiURL =  "https://api.apilayer.com/exchangerates_data/convert?to=USD&from=CAD&amount=1&apikey=BXEh8fPVQeAN2jijFixukfmK2PNmKo83"
+function processForm() {
+
+    
+    console.log(theAmount);
+    console.log(fromCurrency);
+    console.log(toCurrency);
+    formConvert(theAmount,fromCurrency,toCurrency);
+}
+
+async function formConvert(amountInput, fromInput, toInput) {
+
+    const apiURL =  "https://api.apilayer.com/exchangerates_data/convert?to=" + fromInput + "&from=" + toInput + "&amount=" + amountInput + "&apikey=" + currencyAPIKey;
+    
+    console.log(apiURL);
  
     try {
         const response = await fetch(apiURL, {cache: "no-cache"});
@@ -17,7 +35,7 @@ async function getData() {
         if (response.ok) {
             console.log("Convert API result is: " , result);
 
-            let tempAmount = result.info.rate;
+            let tempAmount = result.result;
             console.log("the rate is: " + tempAmount);
             
             outputData(tempAmount);
@@ -29,6 +47,39 @@ async function getData() {
     let 
 }
 }
+
+
+    
+
+
+
+/**
+ *  Run this form on page load
+ */
+
+// async function getData() {
+
+//     const apiURL =  "https://api.apilayer.com/exchangerates_data/convert?to=USD&from=CAD&amount=1&apikey=BXEh8fPVQeAN2jijFixukfmK2PNmKo83"
+ 
+//     try {
+//         const response = await fetch(apiURL, {cache: "no-cache"});
+//         const result = await response.json();
+    
+//         if (response.ok) {
+//             console.log("Convert API result is: " , result);
+
+//             let tempAmount = result.info.rate;
+//             console.log("the rate is: " + tempAmount);
+            
+//             outputData(tempAmount);
+//         }
+
+// } catch (error) {
+//     if (error) throw error;
+//     console.log("Convert API error ", error);
+//     let 
+// }
+// }
 
 
 function outputData(answer) {
