260dbe0d2d722caa67413f0650d2b14f2d54b31b
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2023-05-28 16:50:23 -0700

    Seperate API key into seperate file

---
+++ apikey.js
@@ -0,0 +1,5 @@
+let apiKey = "MpiRfIUCLEYBNP0qMX0B5zdzCsCpJQPV";
+
+// let currencyAPIKey = "BXEh8fPVQeAN2jijFixukfmK2PNmKo83";
+// let currencyAPIKey = "MpiRfIUCLEYBNP0qMX0B5zdzCsCpJQPV";
+// let currencyAPIKey = "6UGvcEOcu5uJ5WMCL0v0kBLyCRFyu5jP";
